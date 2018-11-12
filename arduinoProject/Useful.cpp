4e6f4b9c7862b0473ab2811f4616221955c0e057
Radoslav Figura <radoslav.figura@student.tuke.sk>
2018-11-12 21:21:10 +0100

    Finished docs

--- arduinoProject/Useful.cpp
+++ arduinoProject/Useful.cpp
@@ -6,22 +6,21 @@
 #include "Useful.h"
 #include "Functionality.h"
 
-int calibratedValues[4];
+int calibratedValues[4];                //kalibracne hodnoty pre PR
 
-
-SoftwareSerial BT(0, 1);
-String BluetoothData;
+SoftwareSerial BT(bluetoothRX, bluetoothTX);     //inicializacia objektu bluetoothu
+String BluetoothData;                            //pole pre vstup bluetooth dat
 
 void setupUseful(){
-    BT.begin(9600);
-    BT.println("Bluetooth On");
+    BT.begin(9600);                     //zacne prijmat bluetoothLE data na pasme 9600 baudov
+    BT.println("Bluetooth is on");      //posle bluetooth informaciu ze je zapnuty
 }
 
 void motorVals(int a, int b, int c, int d){
-    digitalWrite(motorA1,a);
-    digitalWrite(motorA2,b);
-    digitalWrite(motorB1,c);
-    digitalWrite(motorB2,d);
+    analogWrite(motorA1,a * 100);
+    analogWrite(motorA2,b * 100);
+    analogWrite(motorB1,c * 100);
+    analogWrite(motorB2,d * 100);
 }
 
 void ledVals(int a, int b){
@@ -46,142 +45,127 @@ void ledLightOnR(){
 }
 
 int dist(int i){
-    long duration;
-    int distance;
+    long duration;                      //casova dlzka za ktoru pride zvuk na echo pin
+    int distance;                       //vzdialenost od objektu
+
+    if(i >= 3){                         //ak je ultrasonic vacsi ako 3 ktory neexistuje skonc...
+        return MAX_DISTANCE + 1;        //vrat hodnotu ktora neovlyvnuje otacanie kolies
+    }
+
+    digitalWrite(trig1Pin+i, LOW);      //vycisti trigger
+    delayMicroseconds(2);               //pocka 2 microsekundy
  
-    digitalWrite(trig1Pin+i, LOW);
-    delayMicroseconds(2);
+    digitalWrite(trig1Pin+i, HIGH);     //zacne posielat signal cez trigPin
+    delayMicroseconds(10);              //Pocka 10 mikrosekund
+    digitalWrite(trig1Pin+i, LOW);      //vycisti trigger
  
-    digitalWrite(trig1Pin+i, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig1Pin+i, LOW);
- 
-    duration = pulseIn(echo1Pin+i, HIGH);
+    duration = pulseIn(echo1Pin+i, HIGH); //zisti ako dlho trvalo zvuku dostat sa naspat na echo pin
  
  
-    distance = duration*0.034/2;
+    distance = duration*0.034/2;        //vypocita vzdialenost od predmetu ako cas za ktory sa zvuk dostal
+                                        // spat na echo vzdialenost dostane vynasobenim casu rychlostou za sekundu
+                                        //dostane ale vydialenost tam a naspat takze tuto vydialenost musi vydelit dvoma
   
-    return distance;
+    return distance;                    //vrati vzdialenost od objektu
 }
 
 void btValues(){
-  if(BT.available()){
+  if(BT.available()){                   //Ak je bluetooth dostupny tak zacni s bt
       BluetoothData += BT.readString();
-      delay(100); 
-  }  
-
-  if(BluetoothData.equals("w")){ // auticko do predu
-      goStraight();
-      BluetoothData=("");
-  }
-  
-  else if (BluetoothData.equals("s")){ //auticko do zadu
-      goReverse();
-      BluetoothData=("");
+      delay(100);
   }
 
-  else if (BluetoothData.equals("d")){ //auticko do prava
-      turnRight(1);
-      BluetoothData=("");
-  }
-
-  else if (BluetoothData.equals("a")){ // auticko do lava
-      turnLeft(1);
-      BluetoothData=("");
-  }
-
-  else if (BluetoothData.equals("p")){   // auticko stop
-      stopCar();
-      BluetoothData=("");
-  }
-
-  else if (BluetoothData.equals("o")){ //auticko autonom
-     goAutonomous();
-     BluetoothData=("");
-  }
-
-  else if (BluetoothData.equals("l")){ // auto turn on led 
-     ledLightOn();
-     BluetoothData=("");
-  }
-
-  else if (BluetoothData.equals("k")){ //turn off led 
-    ledLightOff();
-    BluetoothData=("");
-  }
-  
-  else if (BluetoothData.equals("h")){ // piezo horn
-    horn();
-    BluetoothData=("");
-  }
-
-  else if (BluetoothData.equals("j")){ // go PR
-    readPRData();
-    BluetoothData=("");
+  if(BluetoothData.equals("w")){        // auticko do predu
+        goStraight();
+        BluetoothData=("Going in Straight line");
+  }else if (BluetoothData.equals("s")){ //auticko do zadu
+        goReverse();
+        BluetoothData=("Going in Reverse");
+  }else if (BluetoothData.equals("d")){ //auticko do prava
+        turnRight(true);
+        BluetoothData=("Turning Right");
+  }else if (BluetoothData.equals("a")){ // auticko do lava
+        turnLeft(true);
+        BluetoothData=("Turning Left");
+  }else if (BluetoothData.equals("p")){ // auticko stop
+        stopCar();
+        BluetoothData=("Car is stopped");
+  }else if (BluetoothData.equals("o")){ //auticko autonom
+        goAutonomous();
+        BluetoothData=("Self-driving");
+  }else if (BluetoothData.equals("l")){ //auto turn on led
+        ledLightOn();
+        BluetoothData=("Leds are On");
+  }else if (BluetoothData.equals("k")) {//turn off led
+        ledLightOff();
+        BluetoothData = ("Leds Are OFF");
+  }else if (BluetoothData.equals("h")){ //piezo horn
+        horn();
+        BluetoothData=("I buzz");
+  }else if (BluetoothData.equals("j")){ //go PR
+        readPRData();
+        BluetoothData=("Read PR data");
   }
 }
 
 void calibrate(){
-  int value = 0;
-  int values[4] = {0,0,0,0};
-  while(value < 2500){
-    value++;
-    if(value % 200 <= 100){
-        ledLightOn();
-    }else{
-        ledLightOff();
+    int value = 0;                      //hodnota na prechadzanie while
+    int values[4] = {0,0,0,0};          //zakladne pomocne hodnoty na citanie
+    while(value < 2500){                //vykona 2500krat
+        value++;                        //zvysuje hodnotu kazdu 0.001s
+        if(value % 200 <= 100){         //esteticky blikaj svetlami
+            ledLightOn();               //zapni svetla
+        }else{
+            ledLightOff();              //vypni svetla
+         }
+        for(int i = 0; i < 4; i++){     //prechadza vsetkymi styroma PR a hlada nove hodnoty na kalibraciu
+            values[i] = analogRead(frontRightPR+i);     //cita pomocne hodnoty
+            if(calibratedValues[i] < values[i]){        //ked je hodnota pomocnej vacsia ako kalibracne hodnoty tak zmeni
+                calibratedValues[i] = values[i];        //zmeni kalibracne hodnoty
+            }
+        }
+        delay(1);                       //pocka 0.001s
     }
-    for(int i = 0; i < 4; i++){
-      values[i] = analogRead(frontRightPR+i);
-      if(calibratedValues[i] < values[i]){
-        calibratedValues[i] = values[i];
-      }
-    }
-  }
 }
 
 void readPRData(){
-  //calibrate();
-  //sprav pole v setup kde nacita na zaciatku hodnoty
-  //PP,ZP,PL,ZL
-  int values[4] = {0,0,0,0};
+    int values[4] = {0,0,0,0};                            //hodnoty pre citanie PP,ZP,PL,ZL
 
-  //PP ZP PL ZL ERR s 
-  //1  2  3  4  -1  0 
-  int go = -1;
+    int go = -1;                                          //hodnota rozhodovania ktory PR je najvyssi tam pojde
+                                                          //1 - pravy predny
+                                                          //2 - pravy zadny
+                                                          //3 - lavy predny
+                                                          //4 - lavy zadny
+                                                          //-1 - error
+                                                          //0 - vypnute
 
-  int previous = 0; //previous value of light
-  int light = 0;    //how many of the pr have light pointing at them
+    int previous = 0;                                     //Predosla hodnota svetla
+    int light = 0;                                        //na kolko PR svieti svetlo
 
-  //nacita vsetky photorezistory
-  for(int i = 0; i < 4; i++){
-    values[i] = analogRead(frontRightPR+i); //precitaj hodnoty
-      Serial.print("Hodnota PR cislo: ");
-      Serial.print(i+1);
-      Serial.print(" je ");
-      Serial.println(values[i]);
+    for(int i = 0; i < 4; i++){                           //nacita vsetky photorezistory
+        values[i] = analogRead(frontRightPR+i);           //precitaj hodnoty
+        Serial.print("Hodnota PR cislo: ");
+        Serial.print(i+1);
+        Serial.print(" je ");
+        Serial.println(values[i]);
 
+        if(values[i] > calibratedValues[i]){              //ak je hodnota vacsia ako 200 tak svieti nan svetlo
+            light++;                                      //pocet PR ktore splnaju podmienky
+        }
 
-      //ak je hodnota vacsia ako 200 tak svieti nan svetlo
-    if(values[i] > calibratedValues[i]){
-      light++;
+        if(values[i] > previous){                         //vacsia ako predosla hodnota tak zisti kam je najvyhodnejsie ist
+            go = i+1;                                     //zmen na dalsie
+            previous = values[i];                         //nastavi predoslu
+        }
     }
 
-    //zisti kam je najvyhodnejsie ist
-    if(values[i] > previous){   //vacsia ako predosla hodnota tak dalsie je viac
-      go = i+1;         //zmen na dalsie 
-      previous = values[i];   //nastavi predoslu
-    }
-  }
-
-  //na vsetky 4 svieti tak zastav
-  if(light == 4){
-      go = 0; 
+  if(light == 4){                                         //na vsetky 4 svieti tak zastav
+      go = 0;                                             //go nastav na nulu teda auticko zastavi
   }
 /*
-  if(go < 0) error();       //je nejaky problem
-  else if(go == 4) stop();      //na vsetky svieti = stoj
-  else if(go < 2) turnRight();  //na prave svieti chod vpravo
-  else if(go < 4) turnLeft();   //na lave svieti chod vlavo
+  if(go < 0) error();                                     //je nejaky problem
+  else if(go == 0) stop();                                //na vsetky svieti = stoj
+  else if(go < 2) turnRight();                            //na prave svieti chod vpravo
+  else if(go < 4) turnLeft();                             //na lave svieti chod vlavo
 */
 }
--- arduinoProject/Useful.h
+++ arduinoProject/Useful.h
@@ -1,16 +1,78 @@
 #ifndef USEFUL_H_
 #define USEFUL_H_
 
-void motorVals(int a, int b, int c, int d);
-void ledVals(int a, int b);
-void ledLightOff();
-void ledLightOn();
-void ledLightOnL();
-void ledLightOnR();
-int dist(int i);
+/**
+ * Inicializuje bluetooth
+ */
 void setupUseful();
+
+/**
+ * nastavi hodnoty pre motor zapnutim urcitych pinov na hodnotu 100 alebo 0
+ * @param a hodnota pre pravy motor dopredu
+ * @param b hodnota pre pravy motor dozadu
+ * @param c hodnota pre lavy motor dopredu
+ * @param d hodnota pre lavy motor dozadu
+ */
+void motorVals(int a, int b, int c, int d);
+
+/**
+ * zapne alebo vypne urcitu ledku
+ * @param a hodnota pre pravu ledku
+ * @param b hodnota pre lavu ledku
+ */
+void ledVals(int a, int b);
+
+/**
+ * Vypne vsetky ledky
+ */
+void ledLightOff();
+
+/**
+ * zapne vsetky ledky
+ */
+void ledLightOn();
+
+/**
+ * zapne pravu ledku
+ */
+void ledLightOnL();
+
+/**
+ * zapne lavu ledku
+ */
+void ledLightOnR();
+
+/**
+ * zistuje vzdialenost ultaronic senzora od urciteho objektu
+ * @param i cislo ultarasonic senzora
+ * 0 pre lavy
+ * 1 pre pre pravy
+ * 2 pre predny senzor
+ * pre hodnotu 3 a viac vrati hodnotu vacsiu ako MAX_DISTANCE = vyvarovanie sa problemom
+ * @return vzdialenost od objektu
+ */
+int dist(int i);
+
+/**
+ * Pracuje s bluetooth komunikaciou stylom prijmania
+ * jednoduchych bytych dat ak data nepozna bluetooth ignoruje
+ * a odosle
+ */
 void btValues();
+
+/**
+ * Tato funckia nakalibruje hodnoty svetla podla hodnoty
+ * svetla ktore su pre hladanie svetla idealne svetlo zistuje 2.5s
+ * hlada najvyssie hodnoty
+ */
 void calibrate();
+
+/**
+ * Cita PR data a pomoou podmienok vyhodnocuje kam za sa otocit
+ * ked su svieti svetlo napravo ide napravo
+ * ked svieti svetlo vlavo ide vlavo
+ * ked sviet svetlo na vsetky 4 zastavi sa
+ */
 void readPRData();
 
 
