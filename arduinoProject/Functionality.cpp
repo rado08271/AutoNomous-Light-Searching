d78405012959f58983ae8c623e3108f59b629d50
Radoslav Figura <radoslav.figura@student.tuke.sk>
2018-11-12 18:29:18 +0100

    Updated etc

--- arduinoProject/Functionality.cpp
+++ arduinoProject/Functionality.cpp
@@ -6,104 +6,95 @@
 #include "Useful.h"
 #include "Functionality.h"
 
-Servo servo;  
+Servo servo;                            //objekt serva
 
-/**
- * Nastavenie pre setup na pripojenie serva
- */
 void setupFunc(){
-    servo.attach(servoPin);  
+    servo.attach(servoPin);             //pripoji servo na servoPin
 }
 
-/**
- * Funckia sa stara o zvuk pieza
- */
 void horn(){
-    tone(piezoPin,3000,500); //pin, tone, duration
+    tone(piezoPin,3000,500);            //nastavi frekvenciu ktorou bude zvonit na nejaky cas
 }
 
-/**
- * Auticko ide v rovine
- * @param auton
- */
 void goStraight(bool auton){
-    motorVals(1,0,1,0);
-    if(auton){
-        if(dist(2) < MAX_DISTANCE){
-            horn();
-            doUTurn();
+    motorVals(1,0,1,0);                 //hodnoty pravy dopredu ano, lavy depredu ano
+    if(auton){                          //mod autonomity
+        if(dist(2) < MAX_DISTANCE){     //ak je objekt vo vzdialenosti menej ako MAX_DISTANCE tak zatrub a otoc
+            horn();                     //zatrub
+            doUTurn();                  //otoc sa
         }
     }
 }
- 
+
 void goReverse(){
-    motorVals(0,1,0,1);
+    motorVals(0,1,0,1);                 //hodnoty pravy zadny ano, lavy zadny ano
 } 
 
 void turnLeft(bool straight){
-    for(int i = 90; i > giveTurnValue(dist(0),dist(1)); i--){
-        servo.write(i);
-        if(straight){
-            ledLightOnL();
-            goStraight();
-        }else goReverse();
-        delay(del);
+    for(int i = 90; i < 120; i++) {     //postupne otaca servo dolava
+        if(straight){                   //ked ma ist dopredu tak ide dopredu
+            if(i%10>5){                 //zapne blinker
+                ledLightOnL();
+            }else{
+                ledLightOff();
+            }
+            goStraight(false);          //chod rovno bez zistovania objektov vpredu
+        }else goReverse();              //ked ma ist dozadu ide dozadu
+        servo.write(i);                 //zapis na servo
+        delay(del);                     //pockaj nejaky cas
     }
 }
- 
+
 void turnRight(bool straight){
-    for(int i = 90; i < 120; i++){
-        servo.write(i);
-        if(straight){
-            ledLightOnR();
-            goStraight();
-        }else goReverse();
-        delay(del);
+    for(int i = 90; i > 60; i--) {      //postupne otaca servo doprava
+        if(straight) {                  //ked ma ist dopredu tak ide dopredu
+            if (i % 10 > 5) {           //zapne blinker
+                ledLightOnR();
+            } else {
+                ledLightOff();
+            }
+            goStraight(false);          //chod rovno bez zistovania objektov vpredu
+        }else goReverse();              //ked ma ist dozadu ide dozadu
+
+        servo.write(i);                 //zapis na servo
+        delay(del);                     //pockaj nejaky cas
     }
 }
 
 void doUTurn(){
-    goReverse();
-    delay(1000);
-    for(int i = 90; i > 60; i--) {
-        servo.write(i);
-        delay(del);
-    }
- 
-    for(int i = 90; i < 130; i++) {
-        servo.write(i);
-        delay(del);
-    }
- 
-    goStraight();
+    bool goRight = dist(0) > dist(1) ? true : false; //rozhoduje do ktorej strany ma auticko ist
+
+    goRight ? turnLeft(true) : turnRight(true);   //rozhoduje sa vpravo alebo vlavo a dozadu
+    goRight ? turnRight(false) : turnRight(false);//ak siel vpravo doazdu musi ist dopredu vlavo a naopek
+
     delay(1000);
 }
 
 void stopCar(){
-    motorVals(0,0,0,0);
-    servo.write(90);
+    motorVals(0,0,0,0);                 //zastavi auticko
+    servo.write(90);                    //otoci kolesa dopradu
 }
 
 int giveTurnValue(int leftDistance, int rightDistance){
-    int angle = 90;
-    if(leftDistance > MAX_DISTANCE && rightDistance > MAX_DISTANCE){
-        servo.write(angle);
-        return angle;
+    int angle = 90;                     //nastavi zakladny uhol na 90
+    if(leftDistance > MAX_DISTANCE && rightDistance > MAX_DISTANCE){    //ak su objekty v dostatocnej vzdialenosti chod rovno
+        servo.write(angle);             //ide rovno
+        return angle;                   //vracia uhol
     }
-    bool goRight = (leftDistance < rightDistance) ? true : false;
- 
-    if(goRight){
+    bool goRight = (leftDistance < rightDistance) ? true : false;       //ak je objekt vpravo blizsie ako objekt vlavo tak rozhoduj pre pravu stranu
+
+    if(goRight){                       //ak je vyhodnejsie ist vpravo tak zisti o aky uhol ma nastavit vzdialenost doprava
         angle += (30 - leftDistance);
-    }else{
+    }else{                             //ak je vyhodnejsie ist vlavo tak zistuj o aky uhol ma nastavit vzdialenost dolava
         angle -= (30 - rightDistance);
     }
-    servo.write(angle);
+    servo.write(angle);                //zapise uhol na servo
     
-    return angle;
+    return angle;                      //vracia uhol
 }
 
 void goAutonomous(){
-    giveTurnValue(dist(0),dist(1));
-    goStraight(true);
+    giveTurnValue(dist(0),dist(1));    //zisti hodnotu do ktorej otocit kolesa
+    goStraight(true);                  //chod rovno a zistuj ci je nieco vpredu
 }
 
--- arduinoProject/Functionality.h
+++ arduinoProject/Functionality.h
@@ -1,16 +1,67 @@
 #ifndef FUNCTIONALITY_H_
 #define FUNCTIONALITY_H_
 
-void forSetup();
-void goStraight();
+/**
+ * Auticko ide rovno v jednom smere
+ * @param auton hovori o tom ci je auto
+ * v mode autonomity alebo nieje ak je, tak
+ * pozera v akej vzdialenosti je objekt ak je v dostatocnej
+ * blizkosti tak sa urobi otocku
+ */
+void goStraight(bool auton);
+
+/**
+ * Auticko ide dozadu v jednom smere
+ */
 void goReverse();
+
+/**
+ * Otcoi servo motor do lava
+ * @param straight rozhoduje o tom ci ide auto vpred alebo dozadu pocas tocenia serva
+ */
 void turnLeft(bool straight);
+
+/**
+ * Otcoi servo motor do prava
+ * @param straight rozhoduje o tom ci ide auto vpred alebo dozadu pocas tocenia serva
+ */
 void turnRight(bool straight);
+
+/**
+ * ak je pred autom predmet tak sa auticko otoci
+ * auticko sa rozhodne do ktorej strany je vyhodnejsie ist
+ * ak je vpravo viac miesta ako vlavo tak ide doprava a dozadu
+ * inak ide dolava...
+ */
 void doUTurn();
+
+/**
+ * zastav auticko a otoc kolesa vpred
+ */
 void stopCar();
+
+/**
+ * tato funckia zisti aky uhol sa ma nastavit
+ * pre kolesa rozhoduje sa podla vzdialenosti
+ * @param leftDistance vzdialenost najblizsieho objektu vlavo
+ * @param rightDistance vzdialenost najblizsieho objektu vpravu
+ * @return vracia uhol ktorym sa maju natocit kolesa
+ */
 int giveTurnValue(int leftDistance, int rightDistance);
+
+/**
+ * Funckia sa stara o zvuk pieza
+ */
 void horn();
+
+/**
+ * Nastavenie pre setup na pripojenie serva
+ */
 void setupFunc();
+
+/**
+ * funkcia ktora sa stara o samostatnu autonomitu auta
+ */
 void goAutonomous();
 
 #endif
