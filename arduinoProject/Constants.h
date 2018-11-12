e2f774b278c4b8cae838d9bab564148733eb167a
Radoslav Figura <radoslav.figura@student.tuke.sk>
2018-11-12 21:29:00 +0100

    Constants docs

--- arduinoProject/Constants.h
+++ arduinoProject/Constants.h
@@ -1,30 +1,28 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
-#define motorA1 9   //IN1 for H-bridge
-#define motorA2 10  //IN2 for H-bridge
-#define motorB1 11  //IN3 for H-bridge
-#define motorB2 12  //IN4 for H-beidge
-#define servoPin A0 //Servo Pin OUTPUT
-#define trig1Pin 2  
-#define trig2Pin 3
-#define trig3Pin 4
-#define echo1Pin 5
-#define echo2Pin 6
-#define echo3Pin 7
-#define ledRight 0
-#define ledLeft 0
-#define IRsensorL 0
-#define IRsensorR 0
-#define piezoPin 0
-#define bluetoothTX 0
-#define bluetoothRX 0
-#define frontRightPR A1
-#define backRightPR A2
-#define frontLeftPR A3
-#define backLeftPR A4
+#define motorA1 9           //IN1 pre H-bridge
+#define motorA2 10          //IN2 pre H-bridge
+#define motorB1 11          //IN3 pre H-bridge
+#define motorB2 12          //IN4 pre H-beidge
+#define servoPin A0         //Servo Pin OUTPUT
+#define trig1Pin 2          //triger Pin ultrasonic
+#define trig2Pin 3          //triger Pin ultrasonic
+#define trig3Pin 4          //triger Pin ultrasonic
+#define echo1Pin 5          //echo Pin ultrasonic
+#define echo2Pin 6          //echo Pin ultrasonic
+#define echo3Pin 7          //echo Pin ultrasonic
+#define ledRight 0          //lava ledka
+#define ledLeft 0           //prava ledka
+#define piezoPin 0          //piezo pin
+#define bluetoothTX 0       //bluetooth tx pin
+#define bluetoothRX 0       //bluetooth rx pin
+#define frontRightPR A1     //pravy predny photoresistor
+#define backRightPR A2      //pravy zadny photoresistor
+#define frontLeftPR A3      //lavy predny photoresistor
+#define backLeftPR A4       //lavy zadny photoresistor
 
-const int del = 5; //Delay for turning wheels
-const int MAX_DISTANCE = 20;
+const int del = 5;          //casova dlzka pre tocenie kolies
+const int MAX_DISTANCE = 20;//maximalna vzdialenost
 
 #endif
